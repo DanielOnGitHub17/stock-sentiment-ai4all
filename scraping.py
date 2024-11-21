de1de63595a1cd603152da66a72f3c55680e6b39
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-11-21 15:18:13 -0600

    changed headlines to a list to support random's sample operation. removed unnecessary imports

--- scraping.py
+++ scraping.py
@@ -1,7 +1,5 @@
 from random import sample
 
-import requests as req
-from bs4 import BeautifulSoup
 from pyfinviz.news import News
 
 
@@ -9,6 +7,6 @@ N_HEADLINES = 25
 
 
 def get_headlines(n=N_HEADLINES) -> list:
-    headlines = News().news_df["Headline"]
+    headlines = list(News().news_df["Headline"])
     n_headlines = sample(headlines, n) if len(headlines) > n else headlines
     return n_headlines
