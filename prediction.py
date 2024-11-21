05e0e1e659299277be2612ed873d442c2db603d0
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-11-21 16:11:47 -0600

    wrote prediction api to convert headlines to vectors another function to predict based on model

--- prediction.py
+++ prediction.py
@@ -1,9 +1,25 @@
 """Module to predict stock from data"""
+import re
 import joblib
+import pandas as pd
 
-def predict(data):
-    clf = joblib.load("stock-sentiment-predict.pkl")
-    return clf.predict(data)
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
 
-def array_to_input(array):
-    pass
+
+# Load the model - for Python to cache it.
+STOCK_SENTIMENT_PREDICTOR = joblib.load("stock-sentiment-predict.pkl")
+
+def predict(vectorized_headlines):
+    """Function to predict using model"""
+    stock_model = STOCK_SENTIMENT_PREDICTOR
+    prediction = stock_model.predict(vectorized_headlines)
+    return prediction
+
+def headlines_to_input(headlines):
+    # Make count vectorizer
+    countvector = CountVectorizer(ngram_range=(2, 2))
+    # Combine headlines, make lower case, remove anything that's not text
+    headlines_combined = [re.sub(r"[^a-zA-Z]", ' ', ' '.join(headlines).lower())]
+    vectorized_headlines = countvector.fit_transform(headlines_combined)
+    return vectorized_headlines
