35f69187722c0992e07652c7c4a6bfe959b84a13
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-11-21 13:52:21 -0600

    made script to get finviz page. function to scrape for headline text function to return n scraped headlines, wrapping previous functions

---
+++ scraping.py
@@ -0,0 +1,28 @@
+from random import sample
+
+import requests as req
+from bs4 import BeautifulSoup
+
+
+N_HEADLINES = 25
+FINVIZ_STOCK_URL = "https://finviz.com/news.ashx?v=3"
+
+
+def get_finviz_page() -> str:
+    with req.get(FINVIZ_STOCK_URL) as response:
+        return response.text
+    return "404"
+
+
+def extract_headlines(page_text) -> list:
+    soup = BeautifulSoup(page_text, 'html.parser')
+    headline_elements = soup.select('.nn-tab-link')
+    headlines = [element.text for element in elements]
+    return headlines
+
+
+def get_headlines(n=N_HEADLINES) -> list:
+    page_text = get_finviz_page()
+    headlines = extract_headlines(page_text)
+    n_headlines = sample(headlines, n) if len(headlines) > n else headlines
+    return n_headlines
