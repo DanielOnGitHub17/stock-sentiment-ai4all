7fa2535b0f0eabc461e5b9d641d5c674087b1a2b
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-11-21 15:25:48 -0600

    made columns have linebreaks every 3 columns

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import math
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -7,20 +9,24 @@ from scraping import get_headlines
 from prediction import predict
 
 N_HEADLINES = 25
-
+LINE_BREAK_COUNT = 3
 st.title("Market prediction for today")
 st.markdown("""
 Model that can provide stock market overview based on top news headlines
 """)
 
 st.header("News Headlines for today")
+count = 0
 headlines = get_headlines(N_HEADLINES)
 columns = st.columns(N_HEADLINES)
 for col, headline in zip(columns, headlines):
     with col:
         st.text(headline)
+    count += 1
+    if count % LINE_BREAK_COUNT == 0:
+        st.text("")
 
-st.text('')
+st.text("")
 if st.button("Generate stock overview"):
     # Go to finviz, scrape for input. Feed to model return answer as binary type.
     result = predict(
